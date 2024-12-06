e534c1a3dadd1a0df68612ba8e3be0b0f114f56b
renan <renanrosaschlemper@gmail.com>
2024-12-06 14:42:57 -0300

    update env

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,8 @@
 import { z } from 'zod'
 
+// Schema de validação de variáveis de ambiente
 const envSchema = z.object({
   DATABASE_URL: z.string().url(),
-  API_BASE_URL: z.string().url(),
-  WEB_BASE_URL: z.string().url(),
   PORT: z.coerce.number().default(3333),
 })
 
--- src/server.ts
+++ src/server.ts
@@ -14,19 +14,9 @@ import { getResumoChamados } from './routes/get-service-order-resume'
 import { getAllContractCounters } from './routes/get-all-contract-counters'
 import { getAmountServiceOrder } from './routes/get-amount-service-order'
 import { listTypeContractCounters } from './routes/get-type-contract-counters'
+import { env } from './env'
 
-// Instância Fastify
 const app = fastify()
-
-app.register(getCompanyTechnical)
-app.register(getLinkedParts)
-app.register(getCompanies)
-app.register(getClients)
-app.register(getStatus)
-app.register(getResumoChamados)
-app.register(getAllContractCounters)
-app.register(getAmountServiceOrder)
-app.register(listTypeContractCounters)
 
 // CORS
 app.register(fastifyCors, {
@@ -38,7 +28,17 @@ app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 app.setErrorHandler(errorHandler)
 
-// Instância API
-app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
+// ROTAS
+app.register(getCompanyTechnical)
+app.register(getLinkedParts)
+app.register(getCompanies)
+app.register(getClients)
+app.register(getStatus)
+app.register(getResumoChamados)
+app.register(getAllContractCounters)
+app.register(getAmountServiceOrder)
+app.register(listTypeContractCounters)
+
+app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server is running')
 })
