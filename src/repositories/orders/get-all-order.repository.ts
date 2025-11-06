1e154f4516c7f07bdaab929e4cff49c0392448d1
renan <renanrosaschlemper@gmail.com>
2025-11-06 11:40:00 -0300

    add close-date in concluded orders

--- src/repositories/orders/get-all-order.repository.ts
+++ src/repositories/orders/get-all-order.repository.ts
@@ -43,16 +43,17 @@ export async function getAllOrdersRepository(
     c.id, c.SEQOS, c.CDEMPRESA, c.empresa_id,
     DATE_FORMAT(c.DTINCLUSAO, '%d/%m/%Y') AS DTINCLUSAO,
     DATE_FORMAT(STR_TO_DATE(CONCAT(c.DTPREVENTREGA, ' ', c.HRPREVENTREGA), '%Y-%m-%d %H:%i:%s'), '%d/%m/%Y %H:%i') AS PREVISAOATENDIMENTO,
-    c.HRPREVENTREGA, c.HRINCLUSAO, c.NMCLIENTE, c.DTPREVENTREGA, c.ID_BASE, c.NMSUPORTEA,
+    c.HRPREVENTREGA, c.HRINCLUSAO, c.DTFECHAMENTO, c.NMCLIENTE, c.DTPREVENTREGA, c.ID_BASE, c.NMSUPORTEA,
     CONCAT(DATE_FORMAT(c.DTINCLUSAO, '%d/%m/%Y'), ' ', DATE_FORMAT(c.HRINCLUSAO, '%H:%i')) AS DTHRINCLUSAO,
     c.ENDERECO, c.NUM, c.COMPLEMENTO, c.BAIRRO, c.CIDADE, c.UF, c.CEP, c.CDCLIENTE,
     c.CONTATO, CONCAT('(', c.DDD, ')', ' ', c.FONE) AS telefone, c.EMAIL,
     c.OBSDEFEITOATS, c.OBSDEFEITOCLI, c.DEPARTAMENTO, c.STATUS,
-    e.SERIE, e.MODELO, e.DEPARTAMENTO, e.LOCALINSTAL, e.PATRIMONIO, c.CDEQUIPAMENTO,
+    e.SERIE, e.MODELO, e.DEPARTAMENTO, e.LOCALINSTAL, e.CDCLIENTEENT, e.PATRIMONIO, c.CDEQUIPAMENTO,
     d.NMDEFEITO,
     t.NMOSTP,
     STR_TO_DATE(CONCAT(c.DTPREVENTREGA, ' ', c.HRPREVENTREGA), '%Y-%m-%d %H:%i:%s') AS previsao_atendimento,
     emp.empresa_fantasia, emp.empresa_nome, c.SEQCONTRATO,
+    cl.FANTASIA AS cliente_entrega_fantasia,
     (
       SELECT ci.CDPRODUTO
       FROM chamados_itens ci
@@ -65,6 +66,7 @@ export async function getAllOrdersRepository(
   INNER JOIN defeitos d ON d.CDDEFEITO = c.CDDEFEITO AND d.ID_BASE = c.ID_BASE
   INNER JOIN chamado_tipos t ON t.CDOSTP = c.CDOSTP AND t.ID_BASE = c.ID_BASE
   INNER JOIN empresas emp ON emp.id = c.empresa_id
+  LEFT JOIN clientes cl ON e.CDCLIENTEENT = cl.CDCLIENTE AND e.ID_BASE = cl.ID_BASE AND e.empresa_id = cl.empresa_id
   WHERE c.NMSUPORTET = '${idTecnico}'
     AND c.TFLIBERADO = 'S'
     AND c.ID_BASE = ${idBase}
@@ -110,6 +112,7 @@ export async function getAllOrdersRepository(
         openDate: chamado.DTINCLUSAO || null,
         hourOpen: chamado.HRINCLUSAO,
         prevDate: chamado.PREVISAOATENDIMENTO,
+        closeDate: chamado.DTFECHAMENTO || null,
         status: chamado.STATUS,
         cdstatus: chamado.CDSTATUS,
         cdempresa: chamado.empresa_id,
@@ -136,6 +139,7 @@ export async function getAllOrdersRepository(
           model: chamado.MODELO || null,
           obsdefectcli: chamado.OBSDEFEITOCLI || null,
           obsdefectats: chamado.OBSDEFEITOATS || null,
+          cliente_entrega_fantasia: chamado.cliente_entrega_fantasia || null,
         },
         parts: partsData.parts, // array de peças vinculadas
       }
