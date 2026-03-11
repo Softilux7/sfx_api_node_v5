554d1a85081abf2c3a1c2ec7e9d1d1938c1475c0
renan <renanrosaschlemper@gmail.com>
2026-03-11 10:33:24 -0300

    add nmlocestoque to linked parts

--- src/repositories/orders/get-linked-parts-repositorie.ts
+++ src/repositories/orders/get-linked-parts-repositorie.ts
@@ -3,12 +3,15 @@ import { prisma } from '../../lib/prisma'
 export async function getLinkedParts(idBase: number, seqOs: number) {
   const query = `
     SELECT
-        ci.QUANTIDADE, ci.CDPRODUTO, p.NMPRODUTO 
+        ci.QUANTIDADE, ci.CDPRODUTO, p.NMPRODUTO, el.NMLOCESTOQUE
     FROM
         chamados_itens ci
     INNER JOIN produtos p ON
         p.ID_BASE = ci.ID_BASE
         and p.CDPRODUTO = ci.CDPRODUTO
+    LEFT JOIN estoque_local el ON
+        el.ID_BASE = ci.ID_BASE
+        and el.CDLOCESTOQUE = ci.CDLOCESTOQUE
     WHERE
         ci.ID_BASE = ${idBase}
         and ci.SEQOS = ${seqOs}
@@ -22,7 +25,10 @@ export async function getLinkedParts(idBase: number, seqOs: number) {
     quantidade: peca.QUANTIDADE,
     cdproduto: peca.CDPRODUTO,
     nmproduto: peca.NMPRODUTO || 'Produto não especificado',
+    nmlocestoque: peca.NMLOCESTOQUE || '',
   }))
+
+  console.log('Peças vinculadas:', parts)
 
   return { parts }
 }
