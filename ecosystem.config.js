fa6b3647146cbabfbd2b807cafe214fb1f24469c
Renan-Rosa <79286393+Renan-Rosa@users.noreply.github.com>
2026-03-31 09:58:05 -0300

    update node and add nvm config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,15 @@
-module.exports= {
+module.exports = {
     apps: [
         {
             name: 'API_APP',
             script: 'build/infra/http/server.js',
+            interpreter: "~/.nvm/versions/node/v20.11.0/bin/node",
             env: {
                 NODE_ENV: 'prod',
                 DATABASE_URL: 'mysql://softilux:YceHw7WC5usvP1%23mLtST@10.180.0.8:3306/pws',
                 PORT: 3308,
-                ACCOUNT:'ACe03fc6282bb0c24b829c28dfdcfd6043',
-                AUTH:'5338c9ad4eb5e895bd1dbaae2bc16608',
+                ACCOUNT: 'ACe03fc6282bb0c24b829c28dfdcfd6043',
+                AUTH: '5338c9ad4eb5e895bd1dbaae2bc16608',
             },
             log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
             merge_logs: true,
